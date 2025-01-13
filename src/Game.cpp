17b2b0f37e8e7675dc854fa0b190bd0f07c84e82
David Varshanidze <davidvarshanidze1@gmail.com>
2025-01-13 15:21:10 +0400

    making delay on loop for cpu usage

--- src/Game.cpp
+++ src/Game.cpp
@@ -78,7 +78,10 @@ void Game::Setup() {
 
 void Game::Update() {
   // NOTE: We put delay in Update(); so if we are too fast we waste some time until we reach targeted MILLISEC_PER_FRAME
-  while (!SDL_TICKS_PASSED(SDL_GetTicks(), millisecsPreviousFrame + MILLISECS_PER_FRAME));
+  int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
+  if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME) {
+    SDL_Delay(timeToWait);
+  }
   
   millisecsPreviousFrame = SDL_GetTicks();
   
