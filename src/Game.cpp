2f7725714dc65e8f92e5a798086d566df780ee82
David Varshanidze <davidvarshanidze1@gmail.com>
2025-01-16 17:42:58 +0400

    fixed cpu overheat

--- src/Game.cpp
+++ src/Game.cpp
@@ -47,7 +47,7 @@ void Game::Initialize() {
     return;
   }
   
-  SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+  // SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
 
   isRunning = true; // End
 }
