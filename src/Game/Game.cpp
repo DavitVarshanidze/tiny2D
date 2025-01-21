8dc8e276e487eb3b0a3e67f31dbf1da370e5b1aa
David Varshanidze <davidvarshanidze1@gmail.com>
2025-01-21 14:09:21 +0400

    format

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -5,63 +5,72 @@
 #include <spdlog/spdlog.h>
 #include "Game.h"
 
-Game::Game() {
+Game::Game()
+{
   isRunning = false; // Start
   spdlog::info("Game Constructor Called");
 }
 
-Game::~Game() {
+Game::~Game()
+{
   spdlog::info("Game Destructor Called");
 }
 
-void Game::Initialize() {
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+void Game::Initialize()
+{
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+  {
     spdlog::error("Error initialising SDL");
     return;
   }
-  
+
   SDL_DisplayMode displayMode;
   SDL_GetCurrentDisplayMode(0, &displayMode);
-  
-  windowWidth  = 800;
+
+  windowWidth = 800;
   windowHeight = 600;
   window = SDL_CreateWindow(
-			"Tanks and Helicopters",
-			SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED,
-			windowWidth,
-			windowHeight,
-			0
-  );
-  
-  if (!window) {
+      "Tanks and Helicopters",
+      SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
+      windowWidth,
+      windowHeight,
+      0);
+
+  if (!window)
+  {
     spdlog::error("Error creating SDL window");
     return;
   }
-  
+
   renderer = SDL_CreateRenderer(window, -1, 0);
-  if (!renderer) {
+  if (!renderer)
+  {
     spdlog::error("Error creating SDL renderer");
     return;
   }
-  
+
   // SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
 
   isRunning = true; // End
 }
- 
-void Game::ProcessInput() {
+
+void Game::ProcessInput()
+{
   SDL_Event sdlEvent;
-  while (SDL_PollEvent(&sdlEvent)) {
-    switch(sdlEvent.type) {
-      case SDL_QUIT:
-	isRunning = false;
-	break;
-      case SDL_KEYDOWN:
-	if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
-	  isRunning = false;
-	}
-	break;
+  while (SDL_PollEvent(&sdlEvent))
+  {
+    switch (sdlEvent.type)
+    {
+    case SDL_QUIT:
+      isRunning = false;
+      break;
+    case SDL_KEYDOWN:
+      if (sdlEvent.key.keysym.sym == SDLK_ESCAPE)
+      {
+        isRunning = false;
+      }
+      break;
     }
   }
 }
@@ -69,57 +78,63 @@ void Game::ProcessInput() {
 glm::vec2 playerPosition;
 glm::vec2 playerVelocity;
 
-void Game::Setup() {
+void Game::Setup()
+{
   playerPosition = glm::vec2(10.0, 20.0);
   playerVelocity = glm::vec2(20.0, 0.0);
 }
 
-void Game::Update() {
+void Game::Update()
+{
   // NOTE: We put delay in Update(); so if we are too fast we waste some time until we reach targeted MILLISEC_PER_FRAME
   int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
-  if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME) {
+  if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME)
+  {
     SDL_Delay(timeToWait);
   }
   // NOTE: Difference in ticks since the last frame, converted to seconds
   double deltaTime = (SDL_GetTicks() - millisecsPreviousFrame) / 1000.0f;
-  
+
   millisecsPreviousFrame = SDL_GetTicks();
-  
+
   playerPosition.x += playerVelocity.x * deltaTime;
   playerPosition.y += playerVelocity.y * deltaTime;
 }
 
-void Game::Render() {
+void Game::Render()
+{
   SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
   SDL_RenderClear(renderer);
 
-  SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
-  SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+  SDL_Surface *surface = IMG_Load("./assets/images/tank-tiger-right.png");
+  SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
   SDL_FreeSurface(surface);
 
   SDL_Rect dstRect = {
-    static_cast<int>(playerPosition.x),
-    static_cast<int>(playerPosition.y),    
-    32,
-    32
-  };
-  
+      static_cast<int>(playerPosition.x),
+      static_cast<int>(playerPosition.y),
+      32,
+      32};
+
   SDL_RenderCopy(renderer, texture, NULL, &dstRect);
   SDL_DestroyTexture(texture);
-  
+
   SDL_RenderPresent(renderer);
 }
 
-void Game::Run() {
+void Game::Run()
+{
   Setup();
-  while (isRunning) {
+  while (isRunning)
+  {
     ProcessInput();
     Update();
     Render();
   }
 }
 
-void Game::Destroy() {
+void Game::Destroy()
+{
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
